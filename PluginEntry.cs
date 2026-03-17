da7ed265c8b59ac711e3b693422d41f8e51e2c04
thib <thib@THIB-PC>
2026-03-17 10:35:03 -0300

    botao criado com sucesso

---
+++ PluginEntry.cs
@@ -0,0 +1,19 @@
+﻿// PluginEntry.cs
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(HelloCivil3D.Commands.CriarAlinhamentosCommand))]
+
+namespace HelloCivil3D
+{
+    public class PluginEntry : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            Ribbon.RibbonInitializer.InitializeRibbon();
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
---
+++ src/HelloCivil3D/PluginEntry.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: ExtensionApplication(typeof(HelloCivil3D.PluginEntry))]
+[assembly: CommandClass(typeof(HelloCivil3D.Commands.CriarAlinhamentosCommand))]
+
+namespace HelloCivil3D
+{
+    public class PluginEntry : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            doc?.Editor.WriteMessage("\n[HelloCivil3D] Initialize executado.");
+
+            Ribbon.RibbonInitializer.InitializeRibbon();
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
